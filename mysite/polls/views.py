7aea16c64338663c5bcc701fb4e99abd42fbe3e8
Paul <pauldewilson@gmail.com>
2021-06-30 20:46:49 +0100

    working polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, world! This is the polls index.')
+    question_list = Question.objects.all()
+    question_list_parsed = ', '.join([q.question_text for q in question_list])
+    return HttpResponse(f'Most recent questions: {question_list_parsed}')
+
+def detail(request, question_id):
+    return HttpResponse(f'You are looking at question {question_id}')
+
+def results(request, question_id):
+    return HttpResponse(f'You are looking at the results of question {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting on question {question_id}')
