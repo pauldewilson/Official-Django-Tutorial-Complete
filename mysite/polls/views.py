9928373136222ba37c035caf44fc6390bd5d6211
Paul <pauldewilson@gmail.com>
2021-07-01 01:18:04 +0100

    working templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Question
 
 # Create your views here.
 def index(request):
-    question_list = Question.objects.all()
-    question_list_parsed = ', '.join([q.question_text for q in question_list])
-    return HttpResponse(f'Most recent questions: {question_list_parsed}')
+    # five most recent questions added
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # context which provides to template any variables
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # render template
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse(f'You are looking at question {question_id}')
+    context = {
+        'question_id': question_id,
+    }
+    return render(request,'polls/detail.html', context)
 
 def results(request, question_id):
     return HttpResponse(f'You are looking at the results of question {question_id}')
